cee4deaa8cf7fef86cb3cb987d94e7e8a5970850
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-01-18 10:55:42 +0100

    Add ICUAS24 packages

---
+++ startup/challenge/stop.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# If inside tmux session, get the current session name
+if [[ -n "$TMUX" ]]; then
+  current_session=$(tmux display-message -p '#S')
+fi
+
+# Send Ctrl+C signal to each window of each session
+for session in "${tmux_session_list[@]}"; do
+  # Check if session exists
+  if tmux has-session -t "$session" 2>/dev/null; then
+    # Get the list of windows in the session
+    windows=($(tmux list-windows -t "$session" -F "#{window_index}"))
+    # Iterate through each window and send Ctrl+C
+    for window in "${windows[@]}"; do
+      # Send Ctrl+C to the window
+      tmux send-keys -t "$session:$window" C-c
+      sleep 0.1 # Add a small delay to allow the signal to be processed
+    done
+  fi
+done
+
+# Kill all tmux sessions from the list except for the current one
+for session in "${tmux_session_list[@]}"; do
+  if [[ "$session" != "$current_session" ]]; then
+    tmux kill-session -t "$session" 2>/dev/null
+  fi
+done
+
+# Kill the current tmux session, if in a tmux session
+if [[ -n "$TMUX" ]]; then
+  tmux kill-session -t "$current_session" 2>/dev/null
+fi
